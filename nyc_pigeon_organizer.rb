37d2fd71a8b16ece4144177b04eb4738b012d18c
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 07:34:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,23 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-  data.flatten!
-  data
-end
+  def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |properties, hashes|
+    hashes.each do |attributes, dog_arrays|
+      dog_arrays.each do |name|
 
-end
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].has_key?(properties)
+          new_hash[name][properties] = []
+        end
+
+        if !new_hash[name][properties].include?(attributes)
+           new_hash[name][properties] << attributes.to_s
+         end
+
+       end
+     end
+   end
+   new_hash
+ end
