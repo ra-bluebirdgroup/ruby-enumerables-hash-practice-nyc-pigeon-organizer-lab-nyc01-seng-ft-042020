866f54dea767c42790887b271195ef14dc44be64
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 06:22:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  new_hash = {}
+  new_hash = []
   # data.flatten
   # data.uniq
-  data.each{|i| new_hash << i }
+  data.each{|i| new_hash << i } 
+  pigeons_by_name = %w{ cat sheep bear }.inject do |memo, word|
+   memo.length > word.length ? memo : word
+end
+longest 
 new_hash
 end
