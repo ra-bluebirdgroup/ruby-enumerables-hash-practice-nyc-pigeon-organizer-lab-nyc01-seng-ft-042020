6b57218567738c0f63f35a9034cf01af663b897a
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 06:40:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   # data.uniq
   data.each{ |attributes|
     attributes.each{ |adj| names << adj}
-  } 
+  }
   names.uniq
 
  pigeons_by_name = {names}.inject do |memo, name|
