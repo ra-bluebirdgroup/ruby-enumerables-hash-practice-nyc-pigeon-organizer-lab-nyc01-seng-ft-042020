482e8fa2e6cc09a70a842ca429b0c353913326b5
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 05:56:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,10 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   # data.flatten
   # data.uniq
-  data.each do |atr| 
-    atr.each do |adj| 
-    new_hash <<  adj.invert 
-   end  
- end  
+  data.each do |atr|
+    atr.each do |adj|
+    new_hash <<  adj.invert
+   end
+ end
  new_hash
 end
