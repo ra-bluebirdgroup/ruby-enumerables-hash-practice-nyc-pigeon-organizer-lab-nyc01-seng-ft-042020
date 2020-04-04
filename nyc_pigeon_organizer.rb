a0b0175fbb27a5a1d26a3f3d9219b0b8c8a08e3f
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 07:31:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,16 @@
   def nyc_pigeon_organizer(data)
-  new_hash = {} 
-  data.each do |properties, hashes| 
-    hashes.each do |attributes, dog_arrays| 
-      dog_arrays.each do |name| 
-        
-        if !new_hash.has_key?(name)  
-          new_hash[name] = {} 
-        end 
+  new_hash = {}
+  data.each do |properties, hashes|
+    hashes.each do |attributes, dog_arrays|
+      dog_arrays.each do |name|
 
-        if !new_hash[name].has_key?(properties) 
-          new_hash[name] = [] 
-        end  
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
 
-        if !new_hash[name][properties].include?[attributes] 
+        if !new_hash[name].has_key?(properties)
+          new_hash[name] = []
+        end
+
+        if !new_hash[name][properties].include?[attributes]
            new_hash[name][properties] << attributes
