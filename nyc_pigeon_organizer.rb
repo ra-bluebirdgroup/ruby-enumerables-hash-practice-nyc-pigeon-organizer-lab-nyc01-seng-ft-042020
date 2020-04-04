b933705fa875e71f09592f5cfed54e13d069d5fc
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 07:03:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,25 @@
-require 'pry'
+# require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  names = []
-  # data.flatten
-  # data.uniq
-  data.each{ |attributes|
-      attributes.to_s
-      names << attributes
-    }
+  def nyc_pigeon_organizer(data)
+  # write your code here!
+  new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
 
-  binding.pry
- #pigeons_by_name = {names}.inject do |memo, name|
-# pigeons_by_name
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
 
-names
-
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
