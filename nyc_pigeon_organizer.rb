6c43992bb5d752e058677c7039864a3129238ae3
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 06:32:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  names = ""
+  names = []
   # data.flatten
   # data.uniq
-  data.each{|i| names + i }
+  data.each{ |attributes|
+    attributes.each{ |adj| names << adj}
+
+
+
+
+    }
   pigeons_by_name = {names}.inject do |memo, name|
-    memo : name 
+
 end
 pigeons_by_name
 end
